135aa73c25b330f5e55a8dc05f38b27f1dfa4b6f
Luiz Felipe <luiz.f.barboza@aluno.senai.br>
2025-09-05 20:33:37 -0300

    Desafio me JS com contador

---
+++ livro JS/cap04/js/desafio.js
@@ -0,0 +1,21 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+const secreto = Math.round(Math.random()*100)
+let tentativa = 0
+
+frm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    
+    const num = Number(frm.inNum.value)
+
+    if(num == secreto){
+        resp.innerText = `Parabéns! Você acertou com ${tentativa} tentativas`
+    }else if(num > secreto){
+        resp.innerText = `O número secreto é menor que ${num}`
+        tentativa++
+    }else {
+        resp.innerText = `O número secreto é maior que ${num}`
+        tentativa++
+    }
+
+})

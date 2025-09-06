5c00befe9dc5541569043f676f8d0536f0a4f40c
Luiz Felipe <luiz.f.barboza@aluno.senai.br>
2025-09-05 21:00:06 -0300

    Ajuste no contador do programa

--- livro JS/cap04/js/desafio.js
+++ livro JS/cap04/js/desafio.js
@@ -7,15 +7,14 @@ frm.addEventListener("submit", (e) =>{
     e.preventDefault()
     
     const num = Number(frm.inNum.value)
-
+    tentativa++
     if(num == secreto){
         resp.innerText = `Parabéns! Você acertou com ${tentativa} tentativas`
     }else if(num > secreto){
         resp.innerText = `O número secreto é menor que ${num}`
-        tentativa++
+
     }else {
         resp.innerText = `O número secreto é maior que ${num}`
-        tentativa++
     }
 
 })

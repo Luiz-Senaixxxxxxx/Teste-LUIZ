4e156b933064c62c860a092042c700d3bb46623b
Luiz Felipe <luiz.f.barboza@aluno.senai.br>
2025-09-12 19:40:39 -0300

    Solução do professor para o problema das chinchilas

--- livro JS/cap05/js/resp5_b.js
+++ livro JS/cap05/js/resp5_b.js
@@ -4,18 +4,14 @@ const resp = document.querySelector("h3")
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    let resposta = ""
     let chinchila = Number(frm.inChincilas.value)
     const ano = Number(frm.inAno.value)
 
     for (let i = 1; i <= ano; i++) {
 
-        if (chinchila >= 2) {
-            resposta += `${i}º ano:  ${chinchila} Chinchilas\n`
+            resp.innerText += `${i}º ano:  ${chinchila} Chinchilas\n`
             chinchila = chinchila * 3
-        } else {
-            resposta = `Não tem Chinchilas suficientes para fazer uma estimativa`
-        }
+        
     }
-    resp.innerText = resposta
+    
 })

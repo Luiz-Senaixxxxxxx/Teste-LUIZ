d53fb391934de1fa30b9bdf8201f159ee3f840c3
Luiz Felipe <luiz.f.barboza@aluno.senai.br>
2025-09-29 21:52:19 -0300

    blah

--- livro JS/cap07/js/resp7_c.js
+++ livro JS/cap07/js/resp7_c.js
@@ -5,7 +5,7 @@ const resp2 = document.querySelector("#outResp2")
 frm.addEventListener("submit", (e) =>{
     e.preventDefault()
 
-    const dataInfracao = frm.inDataInfracao.value
+    const dataInfracao = frm.inDataInfracao.value.split("-")
     const valorMulta = Number(frm.inValorMulta.value)
     const dataDesconto = new Date()
 
@@ -15,10 +15,10 @@ frm.addEventListener("submit", (e) =>{
 
     //dataDesconto += new Date (1000 * 60 * 60 * 24 * 90)
 
-    resp1.innerText = " Data Limite oara Pagamento com Desconto:  "
+    resp1.innerText = " Data Limite para Pagamento com Desconto: "
     resp1.innerText += dataDesconto.getDate().toString().padStart(2, "0")+"/"
     resp1.innerText += (dataDesconto.getMonth()+1).toString.padStart(2, "0 ")+"/"
     resp1.innerText += dataDesconto.getFullYear()
-    resp2.innerText = "Valor com desconto R$: " +(valorMulta * 0.8).toFixed(2)
+    resp2.innerText = "Valor com desconto R$: "+(valorMulta * 0.8).toFixed(2)
     
 })

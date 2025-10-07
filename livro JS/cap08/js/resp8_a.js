cef1f854dd6fbf5829063098d602923117f175f4
Luiz Felipe <luiz.f.barboza@aluno.senai.br>
2025-10-06 21:31:46 -0300

    Desafios do cap 08 resolvidos pelo professor.

--- livro JS/cap08/js/resp8_a.js
+++ livro JS/cap08/js/resp8_a.js
@@ -5,26 +5,32 @@ frm.addEventListener("submit", (e) => {
     e.preventDefault()
     const nome = frm.inNome.value
     const idade = Number(frm.inIdade.value)
-
-    categorizarAtleta()
+    resp.innerText = nome+"\n"
+    resp.innerText += retornarTracos(nome) +"\n"
+    resp.innerText += "Categoria: "+  categorizarAtleta(idade)
+    
 })
 
 function retornarTracos(nome){
-    const nomes = frm.inNome.value
-    for(const nome of nomes){
-        nome.join("-")
+    let retorno = ""
+    for(const letra of nome){
+        if(letra != " "){
+            retorno += "-"
+        }else{
+            retorno += " "
+        }
     }
+    return retorno
 }
 
 function categorizarAtleta(idade){
-
-  //  const idade = Number(frm.inIdade.value)
-
+    let retorno = ""
     if(idade < 13 && idade > 0){
-        resp.innerText = "Infantil"
+        retorno = "Infantil"
     }else if(idade >= 13 && idade <= 18){
-        resp.innerText = "Juvenil"
+        retorno= "Juvenil"
     }else{
-        resp.innerText = "Adulto"
+        retorno = "Adulto"
     }
+    return retorno
 }

49bb0f2d71d1d07b7965a612868205377ccb1f80
Luiz Felipe <luiz.f.barboza@aluno.senai.br>
2025-10-09 21:30:31 -0300

    Aula 09/10/2025. Projeto lista de tarefas numa nova Branch

--- Exemplos/lista-tarefas/js/script.js
+++ Exemplos/lista-tarefas/js/script.js
@@ -9,15 +9,16 @@ frm.addEventListener("submit", (e) => {
     const item = frm.inItem.value
     const status = frm.inStatus.value
     const index = frm.inIndex.value
-    index == "" ? lsItem.push({ item, status }) : lsItem[index] = { item, status }
-    atualizarTabela()
-
+    const lineNumber = frm.lineNumber.value
+    // incluir ou atualizar
+    index == "" ? incluir({ item, status }) : atualizar(lineNumber, { item, status }, index)
 })
 
 function prepararEdicao(index) {
     frm.inItem.value = lsItem[index].item
     frm.inStatus.value = lsItem[index].status
     frm.inIndex.value = index
+    frm.lineNumber.value = lsItem[index]._lineNumber
     frm.btApagar.disabled = false
 }
 
@@ -30,8 +31,11 @@ frm.btApagar.addEventListener("click", () => {
     if (confirm("Deseja realmente apagar esse item?") == false) {
         return
     }
-    lsItem.splice(index, 1)
-    atualizarTabela()
+    const lineNumber = frm.lineNumber.value
+    deleteRow(lineNumber).then(() => {
+        lsItem.splice(index, 1)
+        atualizarTabela()
+    })
 })
 
 function atualizarTabela() {
@@ -56,6 +60,7 @@ function limpar() {
     frm.inItem.value = ""
     frm.inStatus.value = ""
     frm.inIndex.value = ""
+    frm.lineNumber.value = ""
     frm.btApagar.disabled = true
 }
 
@@ -67,7 +72,7 @@ if (localStorage.getItem("lsItem") != null) {
 const lsFiltro = frm.querySelectorAll('input[type="checkbox"]')
 for (const bt of lsFiltro) {
     bt.addEventListener("click", filtrar)
-    if(filtro.includes(bt.value)){
+    if (filtro.includes(bt.value)) {
         bt.checked = true
     }
 }
@@ -79,5 +84,57 @@ function filtrar() {
         filtro += bt.checked ? bt.value + "," : ""
     }
     atualizarTabela()
-    localStorage.setItem("filtro",filtro)
+    localStorage.setItem("filtro", filtro)
+}
+
+async function getData() {
+    const response = await fetch("https://api.zerosheets.com/v1/gpo");
+    const data = await response.json();
+    return data;
+}
+
+getData().then((ls) => {
+    lsItem = ls
+    atualizarTabela()
+})
+
+async function createRow(payload) {
+    const response = await fetch("https://api.zerosheets.com/v1/gpo", {
+        method: "POST",
+        body: JSON.stringify(payload)
+    });
+    const data = await response.json();
+
+    return data;
+}
+
+function incluir(obj) {
+    createRow(obj).then((o) => {
+        lsItem.push(o)
+        atualizarTabela()
+    })
+}
+
+async function patchRow(lineNumber, payload) {
+    const url = "https://api.zerosheets.com/v1/gpo/" + lineNumber;
+    const response = await fetch(url, {
+        method: "PATCH",
+        body: JSON.stringify(payload)
+    });
+    const data = await response.json();
+    return data;
+}
+
+function atualizar(lineNumber, obj, index) {
+    patchRow(lineNumber, obj).then((o) => {
+        lsItem[index] = o
+        atualizarTabela()
+    })
+}
+
+async function deleteRow(lineNumber) {
+    const url = "https://api.zerosheets.com/v1/gpo/" + lineNumber; // lineNumber comes from the get request
+    await fetch(url, {
+        method: "DELETE"
+    });
 }

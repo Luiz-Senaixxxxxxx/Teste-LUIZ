23fc5010c1ff1822975d0c71c52c3f9df117a621
Luiz Felipe <luiz.f.barboza@aluno.senai.br>
2025-10-22 21:46:13 -0300

    Incrementação na página de e-comerce do trabalho final, com ajustes do js

---
+++ Exemplos/E-comerce/js/script.js
@@ -0,0 +1,14 @@
+const original = document.querySelector(".produto")
+const produtos = document.querySelector("#produtos")
+
+produtos.innerHTML = ""
+for (const p of lsProduto) {
+    const clone = original.cloneNode(true)
+    clone.querySelector(".nome").innerText = p.nome
+    clone.querySelector(".descricao").innerText = p.descricao
+    clone.querySelector("img").innerText = p.img
+    clone.querySelector(".valor").innerText = `R$ ${p.valor.toFixed(2)}`
+    produtos.appendChild(clone)
+}
+
+

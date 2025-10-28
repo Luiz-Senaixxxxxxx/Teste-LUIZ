d946f6df990a258873ba67a1a5a0fe7f8e30990e
Luiz Felipe <luiz.f.barboza@aluno.senai.br>
2025-10-27 21:08:06 -0300

    começo do novo e-comerce de livraria

--- Exemplos/E-comerce/js/produtos.js
+++ Exemplos/E-comerce/js/produtos.js
@@ -8,50 +8,50 @@ const lsProduto = [
     {
         nome: "Passeio na Floresta",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        img: "img/vela-02.png",
+        valor: 29
     },
     {
-        nome: "Trio Floresta Negra",
+        nome: "Trio Bom Dia",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        img: "img/vela-03.png",
+        valor: 59
     },
     {
         nome: "Meus Cristais",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        img: "img/vela-04 (1).png",
+        valor: 27.90
     },
     {
         nome: "Campo de Lavanda",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        img: "img/vela-05.png",
+        valor: 49
     },
     {
         nome: "Minha Rotina",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-06.png",
         valor: 39
     },
     {
         nome: "Vida no Campo",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        img: "img/vela-07.png",
+        valor: 37
     },
     {
         nome: "Meu Momento",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        img: "img/vela-08.png",
+        valor: 29
     },
     {
         nome: "Flores Vivas",
         descricao: "vela aromática - 300g",
-        img: "img/vela-01.png",
-        valor: 39
+        img: "img/vela-09.png",
+        valor: 49
     },
     
 ]

6b7461d002d07922911d5473207e46e7d74a86d7
Luiz Felipe <luiz.f.barboza@aluno.senai.br>
2025-10-28 20:08:28 -0300

    Edição das imagens e dos valores para a livraria ficar mais bonita

--- Exemplos/Livraria/js/livros.js
+++ Exemplos/Livraria/js/livros.js
@@ -3,54 +3,54 @@ const lsProduto = [
         nome: "Memórias póstumas de Brás Cubas",
         descricao: "Machado de Assis",
         img: "img/memorias-postumas-de-bras-cuba.jpg",
-        valor: 39
+        valor: 47
     },
     {
         nome: "A hora da estrela",
         descricao: "Clarisse Lispector",
         img: "img/a-hora-da-estrel.jpeg",
-        valor: 39
+        valor: 59
     },
     {
         nome: "Capitães de areia",
         descricao: "Jorge Amado",
         img: "img/capitães de areia.jpeg",
-        valor: 39
+        valor: 36
     },
     {
         nome: "Auto da compadecida",
         descricao: "Ariano Suassuna",
         img: "img/auto-da-compadecida.jpeg",
-        valor: 39
+        valor: 23.90
     },
     {
         nome: "Vestido de noiva",
         descricao: "Nelson Rodrigues",
         img: "img/vestido-de-noiva.jpeg",
-        valor: 39
+        valor: 45.50
     },
     {
         nome: "O idiota",
-        descricao: "Dotoiésvick",
+        descricao: "Dostoiésvski",
         img: "img/o-idiota.jpeg",
-        valor: 39
+        valor: 69.70
     },
     {
         nome: "O cortiço",
         descricao: "Aluísio Azevedo",
         img: "img/ocortiço.jpeg",
-        valor: 39
+        valor: 47.90
     },
     {
         nome: "Encontro com a morte",
         descricao: "Agatha Cristhe",
         img: "img/encontro com a morte.jpeg",
-        valor: 39
+        valor: 25.90
     },
     {
         nome: "Dom Casmurro",
         descricao: "Machado de Assis",
         img: "img/dom-casmurro.jpeg",
-        valor: 39
+        valor: 57.60
     }
 ]

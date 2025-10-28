90502e9bf521689645dde81c2c1f0a5ddc1d9d34
Luiz Felipe <luiz.f.barboza@aluno.senai.br>
2025-10-27 21:46:54 -0300

    Começo da estilização do novo e-comerce de livraria

---
+++ Exemplos/Livraria/js/produtos.js
@@ -0,0 +1,56 @@
+const lsProduto = [
+    {
+        nome: "Memórias póstumas de Brás Cubas",
+        descricao: "Machado de Assis",
+        img: "img/memorias-postumas-de-bras-cuba.jpg",
+        valor: 39
+    },
+    {
+        nome: "A hora da estrela",
+        descricao: "Clarisse Lispector",
+        img: "img/a-hora-da-estrel.jpeg",
+        valor: 39
+    },
+    {
+        nome: "Capitães de areia",
+        descricao: "Jorge Amado",
+        img: "img/capitães de areia.jpeg",
+        valor: 39
+    },
+    {
+        nome: "Auto da compadecida",
+        descricao: "Ariano Suassuna",
+        img: "img/auto-da-compadecida.jpeg",
+        valor: 39
+    },
+    {
+        nome: "Vestido de noiva",
+        descricao: "Nelson Rodrigues",
+        img: "img/vestido-de-noiva.jpeg",
+        valor: 39
+    },
+    {
+        nome: "O idiota",
+        descricao: "Dotoiésvick",
+        img: "img/o-idiota.jpeg",
+        valor: 39
+    },
+    {
+        nome: "O cortiço",
+        descricao: "Aluísio Azevedo",
+        img: "img/ocortiço.jpeg",
+        valor: 39
+    },
+    {
+        nome: "Encontro com a morte",
+        descricao: "Agatha Cristhe",
+        img: "img/encontro com a morte.jpeg",
+        valor: 39
+    },
+    {
+        nome: "Dom Casmurro",
+        descricao: "Machado de Assis",
+        img: "img/dom-casmurro.jpeg",
+        valor: 39
+    }
+]
